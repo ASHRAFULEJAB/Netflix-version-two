2ffb12ec4046cf8c6998a29a1cb0f92c666d085c
Ashraful Islam <19103189@iubat.edu>
2023-05-03 22:46:10 +0600

    deploy-issue

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,25 +1,37 @@
-import { useCallback, useState } from "react";
-// import Input from "../components/Input";
-import { useRouter } from "next/router";
 import axios from "axios";
-import { signIn } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { NextPageContext } from "next";
+import { getSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+
 import Input from "@/components/Input";
 
-const TextEncodingPolyfill = require("text-encoding");
-Object.assign(global, {
-  TextEncoder: TextEncodingPolyfill.TextEncoder,
-  TextDecoder: TextEncodingPolyfill.TextDecoder,
-});
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
 
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 const Auth = () => {
+  const router = useRouter();
+
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+
   const [variant, setVariant] = useState("login");
 
   const toggleVariant = useCallback(() => {
@@ -33,12 +45,15 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        callbackUrl: "/profiles",
+        redirect: false,
+        callbackUrl: "/",
       });
+
+      router.push("/profiles");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password]);
+  }, [email, password, router]);
 
   const register = useCallback(async () => {
     try {
@@ -55,19 +70,13 @@ const Auth = () => {
   }, [email, name, password, login]);
 
   return (
-    <div
-      className="relative h-full w-full bg-[url('/images/hero.jpg')]
-     bg-no-repeat bg-center bg-fixed bg-cover"
-    >
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img src="/images/logo.png" alt="logo" className="h-12" />
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
         </nav>
         <div className="flex justify-center">
-          <div
-            className="bg-black bg-opacity-70 px-16 py-16 self-center 
-          mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full"
-          >
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
               {variant === "login" ? "Sign in" : "Register"}
             </h2>
@@ -83,17 +92,17 @@ const Auth = () => {
               )}
               <Input
                 id="email"
-                onChange={(e: any) => setEmail(e.target.value)}
-                value={email}
                 type="email"
-                label="Email"
+                label="Email address or phone number"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
               />
               <Input
-                id="password"
-                onChange={(e: any) => setPassword(e.target.value)}
-                value={password}
                 type="password"
+                id="password"
                 label="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
             <button
@@ -102,7 +111,6 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
-            {/* Social Media Login */}
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
                 onClick={() => signIn("google", { callbackUrl: "/profiles" })}
@@ -135,4 +143,5 @@ const Auth = () => {
     </div>
   );
 };
+
 export default Auth;
